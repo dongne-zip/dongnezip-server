c83a1077a873d82fdde4cf2e7a31e22d91107301
kayleeha <ampmgrow@gmail.com>
2025-02-21 11:51:14 +0900

    feat/add user and mypage

--- model/User.js
+++ model/User.js
@@ -1,5 +1,3 @@
-"use strict";
-
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     "user",
@@ -34,7 +32,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: true,
         field: "password",
       },
-      userNm: {
+      name: {
         type: DataTypes.STRING(20),
         allowNull: true,
         field: "user_nm",
@@ -50,16 +48,11 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: true,
         field: "profile_img",
       },
-      phone: {
-        type: DataTypes.STRING(20),
-        allowNull: true,
-        field: "phone",
-      },
     },
     {
       freezeTableName: true,
       timestamps: true,
-      paranoid: true, // soft delete
+      // paranoid: true, // soft delete
     }
   );
   return User;
--- routes/user.js
+++ routes/user.js
@@ -2,44 +2,70 @@ const express = require("express");
 const passport = require("passport");
 const UserController = require("../controller/Cuser");
 const authenticateToken = require("../middlewares/jwtAuth");
-
+const upload = require("../config/s3");
 const router = express.Router();
 
-// 회원가입
+// 인증 번호 이메일 전송
+router.post("/sendCode", UserController.sendCode);
+
+// 인증번호 검증
+router.post("/verifyCode", UserController.verifyCode);
+
+// 회원가입 (로컬)
 router.post("/join", UserController.join);
 
+// 비밀번호 찾기
+router.post("/findPw", UserController.findPw);
+
 // 아이디 중복 검사
-router.get("/checkId", UserController.checkId);
-
-// 카카오 로그인
-router.get("/kakao", passport.authenticate("kakao"));
-router.get(
-  "/kakao/callback",
-  passport.authenticate("kakao", {
-    failureRedirect: "/?error=카카오로그인 실패",
-  }),
-  UserController.kakaoLogin
-);
-
-// 구글 로그인
-router.post("/login/google", UserController.googleLogin);
+router.post("/checkId", UserController.checkId);
 
 // 로컬 로그인
-router.post("/login/local", UserController.localLogin);
+router.post("/login/local", (req, res, next) => {
+  passport.authenticate("local", { session: false }, (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(401).json({ message: info.message || "로그인 실패" });
+    }
+
+    req.user = user;
+    UserController.localLogin(req, res, next);
+  })(req, res, next);
+});
 
 // 로그아웃
 router.post("/logout", authenticateToken, UserController.logout);
 
-// 비밀번호 재설정
-router.patch("/changePw", authenticateToken, UserController.changePw);
-
-// 회원 탈퇴
-router.delete("/deleteUser", authenticateToken, UserController.deleteUser);
+// 프로필 이미지 수정
+router.post(
+  "/changeImg",
+  upload.single("profileImg"),
+  UserController.changeImg
+);
 
 // 회원 정보 수정
 router.patch("/changeInfo", authenticateToken, UserController.changeInfo);
 
+// 회원 탈퇴
+router.delete("/deleteUser", authenticateToken, UserController.deleteUser);
+
 // 닉네임 중복 검사
-router.get("/checkNickname", authenticateToken, UserController.checkNickname);
+router.post("/checkNick", authenticateToken, UserController.checkNick);
+
+/* 마이페이지 */
+
+// 마이페이지 메인
+router.get("/mypage", authenticateToken, UserController.mypage);
+
+// 판매 내역
+router.get("soldItems", authenticateToken, UserController.soldItems);
+
+// 구매 내역
+router.get("boughtItems", authenticateToken, UserController.boughtItems);
+
+// 찜 내역
+router.get("LikeItems", authenticateToken, UserController.LikeItems);
 
 module.exports = router;
